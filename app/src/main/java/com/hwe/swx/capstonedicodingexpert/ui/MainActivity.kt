f772632aafa922cf197f8970479b3f3df5e3dcc1
sirwarfox <hweitani@gmail.com>
2020-09-04 22:35:08 +0700

    Add Menu Favorite for Dynamic Feature

--- app/src/main/java/com/hwe/swx/capstonedicodingexpert/ui/MainActivity.kt
+++ app/src/main/java/com/hwe/swx/capstonedicodingexpert/ui/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.hwe.swx.capstonedicodingexpert.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -26,5 +31,22 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.item_favorite->{
+                val uri = Uri.parse("movieapp://favorite")
+                startActivity(Intent(Intent.ACTION_VIEW, uri))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
