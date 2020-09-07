f4f319900c36162acead8a354c86d960c3173caa
sirwarfox <hweitani@gmail.com>
2020-09-07 22:48:27 +0700

    Add Certificate Pinning

--- core/src/main/java/com/hwe/swx/core/di/koin/NetworkModule.kt
+++ core/src/main/java/com/hwe/swx/core/di/koin/NetworkModule.kt
@@ -2,6 +2,7 @@ package com.hwe.swx.core.di.koin
 
 import com.hwe.swx.core.BuildConfig
 import com.hwe.swx.core.data.source.remote.network.ApiService
+import okhttp3.CertificatePinner
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
@@ -13,18 +14,27 @@ import java.util.concurrent.TimeUnit
 
 val networkModule = module {
     single {
-        OkHttpClient.Builder().addInterceptor(object : Interceptor {
-            override fun intercept(chain: Interceptor.Chain): Response {
-                val original = chain.request()
-                val newUrl =
-                    original.url.newBuilder()
-                        .addQueryParameter("api_key", BuildConfig.API_KEY)
-                        .build()
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/HkCBucsA3Tgiby96X7vjb/ojHaE1BrjvZ2+LRdJJd0E=")
+            .add(hostname, "sha256/nKWcsYrc+y5I8vLf1VGByjbt+Hnasjl+9h8lNKJytoE=")
+            .add(hostname, "sha256/r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E=")
+            .build()
 
-                val requestBuilder = original.newBuilder().url(newUrl).build()
-                return chain.proceed(requestBuilder)
-            }
-        }).build()
+        OkHttpClient.Builder()
+            .addInterceptor(object : Interceptor {
+                override fun intercept(chain: Interceptor.Chain): Response {
+                    val original = chain.request()
+                    val newUrl =
+                        original.url.newBuilder()
+                            .addQueryParameter("api_key", BuildConfig.API_KEY)
+                            .build()
+
+                    val requestBuilder = original.newBuilder().url(newUrl).build()
+                    return chain.proceed(requestBuilder)
+                }
+            })
+            .certificatePinner(certificatePinner).build()
     }
     single {
         val retrofit = Retrofit.Builder()
